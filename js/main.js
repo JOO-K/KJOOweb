8b69290b31170c26a49512728af0148f06e76ef3
JOO-K <68034507+JOO-K@users.noreply.github.com>
2021-04-19 13:50:13 -0700

    df

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ let renderer;
 let scene;
 let house;
 
-
-
 function init() {
   container = document.querySelector(".scene");
 
@@ -15,26 +13,16 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color('transparent');
 
+
   //Camera setup    
-  const fov = 35;
+  const fov = 90;
   const aspect = container.clientWidth * 1.0/ container.clientHeight * 1.0;
   const near = 0.1;
-  const far = 1000;
+  const far = 10000;
 
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 30);
-    
-    
-  //GridVis
-    
-    const size = 100;
-    const divisions = 100;
-    const colorGrid = '#28d155';
+  camera.position.set(0, 10, 30);
 
-    const gridHelper = new THREE.GridHelper( size, divisions, colorGrid );
-    scene.add( gridHelper );
-    grid = gridHelper;
- 
   
   //Light setup
   const ambient = new THREE.AmbientLight(0x404040, 2);
@@ -51,68 +39,53 @@ function init() {
 
   container.appendChild(renderer.domElement);
 
+  // CUBE
+		// Skybox texture website http://www.custommapmakers.org/skyboxes.php
+		var geometry = new THREE.CubeGeometry( 8000, 8000, 8000 );
+		var cubeMaterials =
+		[
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/posx.jpg' ), side: THREE.DoubleSide } ), // Right side
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/negx.jpg' ), side: THREE.DoubleSide } ), // Left side
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/posy.jpg' ), side: THREE.DoubleSide } ), // Top side
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/negy.jpg' ), side: THREE.DoubleSide } ), // Bottom side
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/posz.jpg' ), side: THREE.DoubleSide } ), // Front side
+			new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader( ).load( 'images/negz.jpg' ), side: THREE.DoubleSide } ) // Back side
+		];    
+    // Create a MeshFaceMaterial, which allows the cube to have different materials on each face
+		var cubeMaterial = new THREE.MeshFaceMaterial( cubeMaterials );
+		var cube = new THREE.Mesh( geometry, cubeMaterial );
+		scene.add( cube );
+    
+    
+    //Get your video element:
+        const video = document.getElementById('video');
+        video.autoplay = true;
+
+    //Create your video texture:
+        const videoTexture = new THREE.VideoTexture(video);
+        const videoMaterial =  new THREE.MeshBasicMaterial( {map: videoTexture, side: THREE.FrontSide, toneMapped: false} );
+        const videoMaterialb =  new THREE.MeshBasicMaterial( {map: videoTexture, side: THREE.BackSide, toneMapped: false} );
+    //Create screen
+        const screen1 = new THREE.PlaneGeometry(40, 40, 0);
+        const screen2 = new THREE.PlaneGeometry(40, 40, 0);
+        const videoScreen1 = new THREE.Mesh(screen1, videoMaterial,);
+        const videoScreen2 = new THREE.Mesh(screen1, videoMaterialb,);
+        scene.add(videoScreen1);
+        scene.add(videoScreen2);
+    
   //Load Models
   let loader = new THREE.GLTFLoader();
     
   loader.load("obj/ring1.gltf", function(gltf) {
-    scene.add(gltf.scene);
+//    scene.add(gltf.scene);
     ring1 = gltf.scene;
     animate();
   });
-     
-  loader.load("obj/ring2.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    ring2 = gltf.scene;
-    animate();
-  });
-  
-  loader.load("obj/ring3.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    ring3 = gltf.scene;
-    animate();
-  });
-
-  loader.load("obj/ring4.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    ring4 = gltf.scene;
-    animate();
-  });
-
-  loader.load("obj/ring5.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    ring5 = gltf.scene;
-    animate();
-  });
-  
-  loader.load("obj/ring6.gltf", function(gltf) {
-    scene.add(gltf.scene);
-    ring6 = gltf.scene;
-    animate();
-  });
-    
+   
 }
 
 
 function animate() {
-    
-  
-    grid.rotation.x = 60;
-    
-    ring1.rotation.x += .01;
-    ring2.rotation.y += .02;
-    ring3.rotation.y += .04;
-    ring4.rotation.y += .06;
-    ring5.rotation.y += .08;
-    ring6.rotation.y += .1;
-    
-    ring1.rotation.z += .01;
-    ring2.rotation.x += .02;
-    ring3.rotation.x += .04;
-    ring4.rotation.x += .06;
-    ring5.rotation.x += .08;
-    ring6.rotation.x += .1;
-    
-    ring1.rotation.y += .01;
     
     
     
@@ -120,6 +93,8 @@ function animate() {
   renderer.render(scene, camera);
     
 }
+
+
 
 init();
 
